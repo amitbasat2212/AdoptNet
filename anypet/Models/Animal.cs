52bb5bb6a7e883e9ded7196a8e3a5c8e812ef68a
amit ben basat <amitbasat124@gmail.com>
2021-04-28 21:55:08 +0300

    Add project files.

---
+++ anypet/Models/Animal.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace anypet.Models
+{
+    public class Animal
+    {
+        public int MyProperty { get; set; }
+
+    }
+}
